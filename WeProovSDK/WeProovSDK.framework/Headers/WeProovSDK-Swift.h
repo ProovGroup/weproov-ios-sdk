ccae068f4a194f49769dd457ad557c2328e2eeb3
Jean Luc Manceron <jeanluc@weproov.com>
2019-09-25 18:25:27 +0200

    add loader

--- WeProovSDK/WeProovSDK.framework/Headers/WeProovSDK-Swift.h
+++ WeProovSDK/WeProovSDK.framework/Headers/WeProovSDK-Swift.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3fd313a50bed16759ffff193ece37447a0b9784119859cc8ba758281126fc3e0
-size 65961
+oid sha256:7362d5c3065ed933e38dfc0e5a785bd14b16ec29253e133e9516f75f25799e8b
+size 66623
