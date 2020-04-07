02fe8845e2ef0d3f3d7832d1db5a5150946721bc
favre <thomas.favre@save.co>
2020-04-07 17:22:53 +0200

    version:1.3.4

--- WeProovSDK/WeProovSDK.framework/Headers/WeProovSDK-Swift.h
+++ WeProovSDK/WeProovSDK.framework/Headers/WeProovSDK-Swift.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ed85470442101cb6411e15cfe45aa8d1849e0bcd322f50c2bec4823292ed80c9
-size 68793
+oid sha256:6d7ed740932bb15f76bdefe9d7414c2c05de80b5465117f7a08f3839d7cf09f2
+size 69183
