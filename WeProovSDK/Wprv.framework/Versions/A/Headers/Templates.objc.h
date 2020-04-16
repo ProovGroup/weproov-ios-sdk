23c18555928baf6fa220e861162a4dd5df706925
favre <thomas.favre@save.co>
2020-04-16 17:30:20 +0200

    add new version of wprv framework

---
+++ WeProovSDK/Wprv.framework/Versions/A/Headers/Templates.objc.h
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:3ee600d6e7503257fe65e8329e683c10f90ba8eeca87fe6f006ff9c1f52d25dc
+size 2670
---
+++ WeProovSDK/Wprv.framework/Versions/A/Headers/Wprv.objc.h
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b1edd0b4bbad1ead737f89391b4734f7adcef3964807d3238f2bbcdadb345163
+size 488
