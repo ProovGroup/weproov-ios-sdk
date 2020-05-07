247d658f6d096b063133d1ed2b7960133207b51c
favre <thomas.favre@save.co>
2020-05-07 11:01:47 +0200

    fix: get upload url with getpackageapiurl method

--- WeProovSDK/WeProovSDK.framework/Headers/WeProovSDK-Swift.h
+++ WeProovSDK/WeProovSDK.framework/Headers/WeProovSDK-Swift.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bca936eb3d8aefa1d3ca0adc8243c95cff7522a754ed4a7ca429f58ff4e9e2f5
-size 70709
+oid sha256:e4362f248299a64f54bc08f7c065af4bc7fe87db359caddbcefe83b45c823dd2
+size 70713
