aee2f53138ea8add1abae477cc088fc17efb2a5b
favre <thomas.favre@mellonmellon.com>
2020-06-04 16:25:16 +0200

    feat: add dammage annotation

--- WeProovSDK/WeProovSDK.framework/Headers/WeProovSDK-Swift.h
+++ WeProovSDK/WeProovSDK.framework/Headers/WeProovSDK-Swift.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e4362f248299a64f54bc08f7c065af4bc7fe87db359caddbcefe83b45c823dd2
-size 70713
+oid sha256:0e2d3cb256b808c70ebed05cc41c7638f699f7723c004166e152715fd91b4444
+size 71177
